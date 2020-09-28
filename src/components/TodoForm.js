5429ba635fb626d809d1853d4e16d253065dd7ee
Sam Tarullo <sam@Sams-Air.attlocal.net>
2020-09-28 16:19:44 -0500

    added a way to add item to list when you hit enter

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const TodoForm = (props) => {
+
         return (
             <>
-            <input type='text' value={props.state.textInput} onChange={props.handleInputChange} placeholder='Enter a task!' />
+            <input onKeyPress={e=>{
+                if (e.key === 'Enter') {
+                    props.handleAddItem(e);
+                }
+            }} type='text' value={props.state.textInput} onChange={props.handleInputChange} placeholder='Enter a task!' />
             <button onClick={props.handleAddItem}>Create Task</button>
             <button onClick={props.handleClearCompleted}>Clear Completed</button>
             </>
